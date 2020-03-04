9f86a75ea3d6b008b3dedb798431ed4f106fe18f
tanvisannareddy <60807451+tanvisannareddy@users.noreply.github.com>
2020-03-04 11:40:29 +0530

    Initial commit

---
+++ tanvi/lab 2-3/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new string[5];
+            Console.WriteLine("Enter name of cities you want to store");
+            for (int i = 0; i < 5; i++)
+            {
+                str[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Enterred cities are");
+            foreach (Object s in str)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
